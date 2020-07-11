7da3657ebe7885666f97b9065ac5485a9b63e759
William Falcon <waf2107@columbia.edu>
2020-07-11 18:12:55 -0400

    add tests for single scalar return from training

--- pytorch_lightning/trainer/trainer.py
+++ pytorch_lightning/trainer/trainer.py
@@ -973,7 +973,7 @@ class Trainer(
 
         # on multi-gpu jobs we only want to manipulate (download, etc) on node_rank=0, local_rank=0
         # or in the case where each node needs to do its own manipulation in which case just local_rank=0
-        if self.can_prepare_data():
+        if self.can_prepare_data() and not self.use_tpu:
             if self.datamodule is not None:
                 self.datamodule.prepare_data()
             model.prepare_data()
